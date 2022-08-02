991834d2abb3a7742280cf683974fdd3aba221f5
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-08-02 09:21:11 +0800

    update

--- clientapp/src/components/header/HeaderComponent.js
+++
@@ -1,9 +0,0 @@
-const HeaderComponent = () => {
-    return (
-        <div className="m-2">
-            <h2>Unofficial Swinburne GPA Calculator</h2>
-        </div>
-    );
-}
-
-export default HeaderComponent;
